251e7c61eef38d5fed9a9f00a0fe1cbcf2b5e5fa
Johann Heyne <mail@johannheyne.de>
2014-06-30 14:58:32 +0200

    Create yerscriptmediaqueries.js

---
+++ yerscriptmediaqueries.js
@@ -0,0 +1,66 @@
+// YerScriptMediaQueries
+
+function YerScriptMediaQueries() {
+	
+	var t = this;
+	
+	t.defaults = {
+		'resize_treshold': 500,
+		'mediaqueries': undefined,
+	}
+	
+	t.set = undefined;
+	
+	t.init =  function( p ) {
+		
+		t.set = $.extend( true, t.defaults, p );
+		
+		if ( typeof t.set.mediaqueries === 'object' ) {
+
+			t.loop_breakpoints();
+
+			$(window).resize( function() {
+
+				if ( this.resize_timeout ) {
+
+					clearTimeout( this.resize_timeout );
+				}
+
+				this.resize_timeout = setTimeout( function() {
+
+					t.loop_breakpoints();
+
+				}, t.set.resize_treshold );
+
+			});
+
+		}
+	};
+	
+	t.loop_breakpoints =  function() {
+		
+		var window_width = $( window ).width();
+
+		for ( var i in t.set.mediaqueries ) {
+			
+			if ( typeof t.set.mediaqueries[ i ].breakpoint === 'number') {
+			    
+				if ( t.set.mediaqueries[ i ].breakpoint < window_width ) {
+
+					if ( typeof t.set.mediaqueries[ i ].before === 'function' ) {
+
+						t.set.mediaqueries[ i ].before();
+					}
+				}
+				else {
+
+					if ( typeof t.set.mediaqueries[ i ].after === 'function' ) {
+
+						t.set.mediaqueries[ i ].after();
+					}
+				}
+			}
+		}
+	};
+	
+};
